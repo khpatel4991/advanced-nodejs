c60ffa08b0cd72a014b4e771845f7846adfbf024
Kashyap Patel <khpatel4991@gmail.com>
2020-04-07 01:18:26 -0400

    Fix string decoder example in 1.5

--- 1.5/StringDecoder.js
+++ 1.5/StringDecoder.js
@@ -2,11 +2,11 @@ const { StringDecoder } = require('string_decoder');
 const decoder = new StringDecoder('utf8');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk != null) {
-    const buffer = Buffer.from([chunk]);
-    console.log('With .toString():', buffer.toString());
-    console.log('With StringDecoder:', decoder.write(buffer));
+  let chunk;
+  while((chunk = process.stdin.read()) !== null) {
+    const buff = Buffer.from([chunk])
+    console.log('With .toString():', buff.toString());
+    console.log('With StringDecoder:', decoder.write(buff));
   }
 });
 
